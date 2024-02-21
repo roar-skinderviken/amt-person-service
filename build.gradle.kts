f1501b67e0c43e6a2d66b991d147d8b9180cd229
Tormod Fossum <110398791+tofoss@users.noreply.github.com>
2024-02-21 10:34:03 +0100

    Bump postgresql til 42.7.2 (#214)

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,10 +35,9 @@ val confluentVersion = "7.5.1"
 val avroVersion = "1.11.3"
 val jacksonVersion = "2.16.1"
 val micrometerVersion = "1.12.3"
-val postgresVersion = "42.7.2"
 val mockOauth2ServerVersion = "2.1.2"
 
-extra["logback.version"] = "1.4.14"
+extra["postgresql.version"] = "42.7.2"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
@@ -78,8 +77,6 @@ dependencies {
 
 	implementation("net.javacrumbs.shedlock:shedlock-spring:$shedlockVersion")
 	implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:$shedlockVersion")
-
-	runtimeOnly("org.postgresql:postgresql:$postgresVersion")
 
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
